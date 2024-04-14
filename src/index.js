396f4eb3e50a48cd3280fd0088c3d070c059f134
sharon adhiambo <adhiambosharonodhiambo@gmail.com>
2024-04-14 11:49:38 +0300

    add listholder

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 // Your code here
+let URL = 'http://localhost:3000/films'
+
+
+const listHolder = document.getElementById('films')
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementsByClassName('film item')[0].remove()
+    fetchMovies(URL)
+})
