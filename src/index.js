6e3063e0e3be0847234b045646f94abe882de176
sharon adhiambo <adhiambosharonodhiambo@gmail.com>
2024-04-14 11:52:58 +0300

    fetch and display movie

--- src/index.js
+++ src/index.js
@@ -7,3 +7,23 @@ document.addEventListener('DOMContentLoaded', ()=>{
     document.getElementsByClassName('film item')[0].remove()
     fetchMovies(URL)
 })
+
+//Create fetch function to get the data from the db.json
+function fetchMovies(URL){
+    fetch(URL)
+    .then(resp => resp.json())
+    .then(movies => {
+        movies.forEach(movie => {
+            displayMovie(movie)
+        });
+    })
+}
+//function to display the titles of the movies as a list
+function displayMovie(movie){
+   
+    const list = document.createElement('li')
+    list.style.cursor="cell"
+    list.textContent= (movie.title)
+    listHolder.appendChild(list)
+    addClickEvent()
+}
